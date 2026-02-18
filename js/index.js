26a82703ecbd3bc0929c70d2e0955cfde8c568a8
Raz Ebrat <raazebrat91@gmail.com>
2026-02-17 23:46:59 -0500

    fixed footer and skills per reviewer

--- js/index.js
+++ js/index.js
@@ -1,47 +1,31 @@
-/*Add a Footer Elment*/
-console.log("index.js is loaded ✅");
-// 1. select the <body> element
-const body = document.querySelector("body");
+/* ========= FOOTER ========= */
 
-// 2. create a <footer> element
-//const footer = document.createElement("footer");
+// create footer
+const footer = document.createElement("footer");
 
-// 3. Add text inside the footer
-footer.innerHTML = "© 2026 Raz Ebrat";
+// add footer to body
+document.body.appendChild(footer);
 
-// 4. Append footer to the body
-body.appendChild(footer);
-
-/* Insert Copyright Text in Footer */
-
-// 1. create a date obj
+// get current year
 const today = new Date();
-
-// 2. get current year
 const thisYear = today.getFullYear();
 
-// 3. select the footer element from HTML
-const footer = document.querySelector("footer");
-
-// 4. create a paragraph
+// create paragraph
 const copyright = document.createElement("p");
 
-// 5. Add text
-//copyright.innerHTML = `© ${thisYear} Raz Ebrat`;
-
-/* STRETCH GOAL: Unicode copyright symbol (©)*/
+// add dynamic text
 copyright.innerHTML = `\u00A9 ${thisYear} Raz Ebrat`;
 
-// 6. append to the footer
+// add paragraph to footer
 footer.appendChild(copyright);
 
-// 7. create an array of skills
-const skills = ["JavaScript", "HTML", "CSS", "Git", "GitHub"];
+/* ========= SKILLS ========= */
 
-const skillsSection = document.querySelector("#skills");
+const skills = ["HTML", "CSS", "JavaScript", "GitHub"];
+
+const skillsSection = document.getElementById("skills");
 const skillsList = skillsSection.querySelector("ul");
 
-// 8. create a for loop
 for (let i = 0; i < skills.length; i++) {
   const skill = document.createElement("li");
   skill.innerText = skills[i];
